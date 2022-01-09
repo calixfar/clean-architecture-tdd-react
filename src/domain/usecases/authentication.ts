c86ad6532d2c28c7edcba3af7ff946a77f860f0a
calixfar <luis-santiago97@hotmail.com>
2022-01-08 20:37:04 -0500

    feat: add authentication usecase

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,11 @@
+import { AccountModel } from "@/domain/models";
+
+export interface Authentication {
+  auth: (params: Authentication.Params) => Promise<AccountModel>
+}
+
+export namespace Authentication {
+  export type Params = {
+    email: string, password: string
+  }
+}
