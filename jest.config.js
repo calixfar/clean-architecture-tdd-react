00cc4720bad78da3bdd03044c4f0ba4afa6ac0b4
calixfar <luis-santiago97@hotmail.com>
2022-01-08 21:40:35 -0500

    chore: moduleNameWrapper config add for jest

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,9 @@ module.exports = {
   testEnvironment: 'node',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
   }
 }
