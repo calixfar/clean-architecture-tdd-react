a5d7108b6efb6e5986e78e55bc96605c367f547c
luissantiago <luis.santiago@itglobers.com>
2022-01-21 12:31:48 -0500

    refactor: move to Input separate file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Footer, LoginHeader, Spinner } from '@/presentation/components'
+import { Footer, Input, LoginHeader, Spinner } from '@/presentation/components'
 import Styles from './login-styles.scss'
 
 const Login: React.FC = () => {
@@ -8,14 +8,8 @@ const Login: React.FC = () => {
       <LoginHeader />
       <form className={Styles.form}>
         <h2>Login</h2>
-        <div className={Styles.inputWrap}>
-          <input type="email" placeholder="Digite su e-mail" />
-          <span className={Styles.status}>🔴</span>
-        </div>
-        <div className={Styles.inputWrap}>
-          <input type="password" placeholder="Digite su contraseña" />
-          <span className={Styles.status}>🔴</span>
-        </div>
+        <Input type="email" name="email" placeholder="Digite su email" />
+        <Input type="password" name="password" placeholder="Digite su password" />
         <button type="submit">Entrar</button>
         <span className={Styles.link}>Crear cuenta</span>
         <div data-testid="error-wrap" className={Styles.errorWrap}>
