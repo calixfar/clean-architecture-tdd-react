4e9efef051427889ff4b601bacccbd95a0beddbd
luissantiago <luis.santiago@itglobers.com>
2022-01-21 15:21:45 -0500

    feat: add role button to submit

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Footer, FormStatus, Input, LoginHeader, Spinner } from '@/presentation/components'
+import { Footer, FormStatus, Input, LoginHeader } from '@/presentation/components'
 import Styles from './login-styles.scss'
 
 const Login: React.FC = () => {
@@ -10,7 +10,7 @@ const Login: React.FC = () => {
         <h2>Login</h2>
         <Input type="email" name="email" placeholder="Digite su email" />
         <Input type="password" name="password" placeholder="Digite su password" />
-        <button type="submit" className={Styles.submit}>Entrar</button>
+        <button role="button" type="submit" className={Styles.submit}>Entrar</button>
         <span className={Styles.link}>Crear cuenta</span>
         <FormStatus />
       </form>
