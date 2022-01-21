c1b37de1159d435503b8127cf082be1c14754839
luissantiago <luis.santiago@itglobers.com>
2022-01-21 17:01:31 -0500

    refactor: move render login to factory mehod

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import { screen, render } from '@testing-library/react'
 import Login from './login'
 
+type SutTypes = {}
+
+const makeSut = (): SutTypes => {
+  render(<Login/>)
+  return {}
+}
 describe('Login', () => {
   test('should start with initial state', () => {
-    render(<Login/>)
+    makeSut()
     const errorWrap = screen.getByRole('status')
     expect(errorWrap.childElementCount).toBe(0)
     const submitButton = screen.getByRole('button') as HTMLButtonElement
