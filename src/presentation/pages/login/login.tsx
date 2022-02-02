264c71cc4d28915757ae064f33b48971bb04a238
luissantiago <luis.santiago@itglobers.com>
2022-02-02 07:36:56 -0500

    feat: ensure login present spinner on submit

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -26,13 +26,21 @@ const Login: React.FC<Props> = ({ validation }) => {
     })
   }, [state.email, state.password])
 
-  const disabledButton = !!(state.emailError || state.passwordError)
+  const disabledButton = !!state.emailError || !!state.passwordError
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault()
+    setState({
+      ...state,
+      isLoading: true
+    })
+  }
  
   return (
     <div className={Styles.login}>
       <LoginHeader />
       <Context.Provider value={{ state, setState }}>
-        <form className={Styles.form}>
+        <form className={Styles.form} onSubmit={handleSubmit}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite su email" />
           <Input type="password" name="password" placeholder="Digite su password" />
