568b949caa559e816933f6b1d51ef060f839f14b
luissantiago <luis.santiago@itglobers.com>
2022-02-06 11:33:00 -0500

    test: ensure login navigates to main page on success

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -17,7 +17,7 @@ type SutParams = {
   validationError: string
 }
 
-const history = createMemoryHistory()
+const history = createMemoryHistory({ initialEntries: ['/login'] })
 
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub()
@@ -162,6 +162,8 @@ describe('Login', () => {
     const form = screen.getByRole('form')
     await waitFor(() => form)
     expect(localStorage.setItem).toHaveBeenCalledWith('accessToken', authenticationSpy.account.accessToken)
+    expect(history.length).toBe(1)
+    expect(history.location.pathname).toBe('/')
   })
 
   test('should go to signup page', async () => {
